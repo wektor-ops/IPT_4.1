6b1f98af6320e6b243d5892c7110c42b8e447fd5
wektor-ops <wiktor_wisniewski@sluz.ch>
2025-09-05 12:51:00 +0200

    Add vue project

---
+++ IctBerufsbildungZentralschweiz modul-294-aufgaben main data-flow (1)/src/App.vue
@@ -0,0 +1,63 @@
+<script  setup lang="ts">
+import { faker } from '@faker-js/faker'
+import { ref } from 'vue'
+import Post from './components/Post.vue'
+
+const unSortedposts = ref([
+  { id: 1, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 8 },
+  { id: 2, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 0 },
+  { id: 3, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: -4 },
+  { id: 4, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 0 },
+  { id: 5, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: -8 },
+  { id: 6, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 4 },
+  { id: 7, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 6 },
+  { id: 8, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 3 },
+  { id: 9, title: faker.lorem.words(10), text: faker.lorem.paragraph(), user: faker.internet.userName(), votes: 15 },
+])
+const posts = computed(() =>
+    [...unSortedPosts.value].sort((a, b) => b.votes - a.votes)
+);
+function voteUp (newid) {
+  console.log('voteUp', newid)
+  const post = posts.value.find(post => post.id === newid)
+  post.votes++;
+}
+
+function voteDown (newid) {
+  console.log('voteDown', newid)
+  const post = posts.value.find(post => post.id === newid)
+  post.votes--;
+}
+</script>
+
+<template>
+  <div class="container" >
+    <Post v-for="post in posts" :key="post.id"
+        :id="post.id"
+        :title="post.title"
+        :text="post.text"
+        :user="post.user"
+        :votes="post.votes"
+          @voteUp="voteUp($event)"
+          @voteDown="voteDown($event)"
+    />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  background: #f0f0f0;
+}
+
+.container {
+  max-width: 800px;
+  margin: 40px auto;
+  border: 1px solid #fafafa;
+  box-shadow: 0 0 2px rgba(0, 0, 0, .2);
+  background: #fff;
+  border-radius: 4px;
+  position: relative;
+}
+</style>
---
+++ IctBerufsbildungZentralschweiz modul-294-aufgaben main data-flow (1)/src/components/Post.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+defineEmits(['voteUp','voteDown']);
+defineProps<{
+  id: number
+  title?: string
+  text?: string
+  user?: string
+  votes: number
+}>()
+</script>
+
+<template>
+  <div class="post">
+    <div class="post__votes voter">
+      <button class="voter__action" @click="$emit('voteUp', id)">
+        👍
+      </button>
+      <div class="voter__votes">
+        {{ votes }}
+      </div>
+      <button class="voter__action" @click="$emit('voteDown', id)">
+        👎
+      </button>
+    </div>
+    <div class="post__content">
+      <div class="post__user">
+        von {{ user }}
+      </div>
+      <div class="post__title">
+        {{ title }}
+      </div>
+      <div class="post__text">
+        {{ text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.post {
+  display: flex;
+  padding: 20px;
+  border: 2px solid transparent;
+}
+
+.post--trending {
+  border: 2px dashed #ecc500;
+  background: #fff8ee;
+}
+
+.post--spam {
+  opacity: .25;
+}
+
+.post + .post {
+  border-top: 1px solid #fafafa;
+}
+
+.post__user {
+  font-size: .9rem;
+  color: #666;
+  margin-bottom: 10px;
+}
+
+.post__title {
+  font-size: 1rem;
+  line-height: 1.1;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.post__text {
+  color: #666;
+  line-height: 1.2;
+  font-size: .9rem;
+}
+
+.post__votes {
+  width: 40px;
+  margin-right: 20px;
+  display: flex;
+  flex-shrink: 0;
+  flex-direction: column;
+  align-items: center;
+}
+
+.voter__votes {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin: 8px 0;
+}
+
+.voter__action {
+  appearance: none;
+  background: none;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+  font-size: 1.3rem;
+  transition: background .2s ease;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  line-height: 1;
+}
+
+.voter__action:hover {
+  background: #f0f0f0;
+}
+</style>
